7844813a358919aee5b816f265c18c9046a6bb91
Jordan Ball <jordan.ball@willowtreeapps.com>
2021-12-05 17:25:14 -0500

    problem 5

---
+++ Day05/problem1.mjs
@@ -0,0 +1,28 @@
+import * as fs from 'fs';
+import { startUp, objectHolder } from './helpers.mjs';
+
+startUp();
+
+// get the data and put into a form [ {x: val, y: val}, {x: val, y: val}]
+const dataArray = fs.readFileSync('./data.txt',{ encoding: 'utf8', flag:'r' })
+    .split('\n')
+    .cjbxFormatData();
+
+// remove any lines that are not vertical or horizontal
+const filteredLineData = dataArray.cjbxRemoveExcessLines({diagonalsIncluded: false});
+
+filteredLineData.forEach((segment) => {
+    segment.cjbxAddPointsToObjectHolder();
+});
+
+let counter = 0;
+
+let keysArray = Object.keys(objectHolder);
+
+keysArray.forEach((point) => {
+    if (objectHolder[point] >= 2) {
+        counter += 1;
+    } 
+})
+
+console.log('The total count is: ', counter);
